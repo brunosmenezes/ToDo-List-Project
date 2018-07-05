d45dc11c1b0856dbdb6ca4b69911346b476b1ae9
Bruno Menezes <bruno.sinopoli@uol.com.br>
2018-07-04 22:38:47 -0300

    Upload dos Arquivos

---
+++ Todo List Project/assets/js/todos.js
@@ -0,0 +1,27 @@
+
+//Check Off Specifics Todos By Clicking
+$('ul').on("click","li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to Delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//pegar o texto do Input
+		var todoText = $(this).val();
+		$(this).val("");
+		//Criar um novo li e adicionar ao ul
+		$("ul").append("<li><Span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
